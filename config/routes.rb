805a87f1dd2dd6b66bf13b72132ecad7b9d64a32
Danielle Glass <danielleglass@me.com>
2017-09-23 15:22:34 -0700

    create use paths logic in layout for login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # get 'home/index'
-  get '/auth/:provider/callback', to: 'sessions#create'
-  resources :cakerequests
 
   root 'home#index'
+  get 'home/index'
+  # get '/auth/:provider/callback', to: 'sessions#create'
+  resources :cakerequests
+  devise_for :users
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
