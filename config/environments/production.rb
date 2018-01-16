63ad4f6693273b4e3974cb139c4cf816768daa44
DaniGlass <danielleglass@me.com>
2018-01-16 14:58:38 -0800

    set up mailers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,20 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'beachdogbakery.herokuapp.com/'}
+
+  config.action_mailer.smtp_settings = {
+      user_name:      ENV['SENDMAIL_USERNAME'],
+      password:       ENV['SENDMAIL_PASSWORD'],
+      domain:         ENV['MAIL_HOST'],
+      address:       'smtp.gmail.com',
+      port:          '587',
+      authentication: :plain,
+      enable_starttls_auto: true
+    }
+
   # Code is not reloaded between requests.
   config.cache_classes = true
   # config.force_ssl = true
